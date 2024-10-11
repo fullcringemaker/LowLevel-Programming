58f9b93efd01073cf49df787bbec470924995fd0
fullcringemaker <dmitriytrofimenko105@mail.ru>
2024-10-11 16:43:35 +0300

    Update Lab1dop.asm

--- Lab1/Lab1dop.asm
+++ Lab1/Lab1dop.asm
@@ -1,85 +1,113 @@
-assume CS:code, DS:data
+assume cs:code, ds:data
 data segment
-    a db 1                ; a = 1
-    b db 2                ; b = 2
-    d db 4                ; d = 4
-    c db 3                ; c = 3
-    str db '000 00h','$'   
+    a dw 17
+    b dw 2
+    c dw 4
+    d dw 32
+    result dw 0
+    buffer db 6 dup(0)
+    hex_buffer db 5 dup(0)
+    space db ' ', '$'
 data ends
 
 code segment
 start:
-    mov AX, data          
-    mov DS, AX            
-
-    mov al, a     ;al = 1
-    mov bl, b     ;bl = 2
-    mov ah, 0     ;обнуление ah
-    div bl        ;al = 1/2 = 0, ah = 1 mod 2 = 1
-    mov bh, al    ;bh = 0
-
-    mov al, d     ;al = 1
-    mov bl, c     ;bl= 3
-    mov ah, 0 
-    div bl        ;al= 4/3 = 1, ah = 4 mod 3 = 1
-    add bh, al    ;bh = bh+al=0+1=1
+    mov ax, data
+    mov ds, ax
     
-    sub bh, 1     ;bh=bh-1=0
+    mov ax, [a]
+    mov dx, 0
+    mov bx, [b]
+    div bx
+    mov bx, ax
 
+    mov ax, [d]
+    mov dx, 0
+    mov cx, [c]
+    div cx
 
-    mov ax, 0             
-    mov al, bh            
-    mov ah, 0             
-    mov bl, 100           
-    div bl                
-    add al, '0'           
-    mov [str], al         
+    add ax, bx
+    sub ax, 1
+    mov [result], ax
+    
+    mov si, offset buffer
+    add si, 5
+    mov bx, si
+    mov cx, 0
+    mov ax, [result]
+    cmp ax, 0
+    jne dec_convert_loop
+    mov byte ptr [bx], '0'
+    mov byte ptr [bx+1], '$'
+    mov dx, bx
+    mov ah, 09h
+    int 21h
+    jmp space_output
 
-    mov al, ah            
-    mov ah, 0             
-    mov bl, 10            
-    div bl                
-    add al, '0'           
-    mov [str+1], al       
+dec_convert_loop:
+    mov dx, 0
+    mov si, 10
+    div si
+    add dl, '0'
+    mov [bx], dl
+    dec bx
+    inc cx
+    cmp ax, 0
+    jne dec_convert_loop
+    inc bx
+    mov si, cx
+    add si, bx
+    mov byte ptr [si], '$'
+    mov dx, bx
+    mov ah, 09h
+    int 21h
 
-    mov al, ah            
-    add al, '0'           
-    mov [str+2], al       
+space_output:
+    mov dx, offset space
+    mov ah, 09h
+    int 21h
+    jmp hex_output
 
-    mov al, bh     
-    mov ah, 0             
-    mov bl, 16            
-    div bl                
+hex_output:
+    mov si, offset hex_buffer
+    add si, 4
+    mov bx, si
+    mov cx, 0
+    mov ax, [result]
+    cmp ax, 0
+    jne hex_convert_loop
+    mov byte ptr [bx], '0'
+    mov byte ptr [bx+1], '$'
+    mov dx, bx
+    mov ah, 09h
+    int 21h
+    jmp exit_program
 
-    cmp al, 10           
-    jb hex_high_is_num    
-    add al, 55            
-    jmp hex_high_done    
-hex_high_is_num:
-    add al, '0'          
-hex_high_done:
-    mov [str+4], al       
+hex_convert_loop:
+    mov dx, 0
+    mov si, 16
+    div si
+    cmp dl, 9
+    jle hex_digit
+    add dl, 7
+hex_digit:
+    add dl, '0'
+    mov [bx], dl
+    dec bx
+    inc cx
+    cmp ax, 0
+    jne hex_convert_loop
+    inc bx
+    mov si, cx
+    add si, bx
+    mov byte ptr [si], '$'
+    mov dx, bx
+    mov ah, 09h
+    int 21h
 
-    mov al, ah           
-    cmp al, 10            
-    jb hex_low_is_num     
-    add al, 55            
-    jmp hex_low_done     
-hex_low_is_num:
-    add al, '0'           
-hex_low_done:
-    mov [str+5], al
+exit_program:
+    mov ah, 4Ch
+    int 21h
 
-    lea dx, str           
-    mov ah, 09h           
-    int 21h               
-
-    mov AH, 4Ch           
-    int 21h               
 code ends
 end start
-
-Данный код выполняет свою задачу а именно осуществляет вывод ответа в десятиричном формате и, через пробел, в шестнадцатиричном формате, при этом на конце шестнадцатиричного числа была h.Также вывод осуществлялся без никаких дополнительных ненужных надписей или слов, выводится только 2 числа с теми условиями, что я указал в предыдущем предложении. 
-Однако есть один минус. Повтор кусков кода не является оптимальным универсальным методом (к тому же может не работать, если считать, что и переменные, и результат будет большей разрядности, чем байт). 
-
-Мне необходимо переделать данный код с использованием циклов. При этом переделанный вариант должен выполнять свою задачу, которую я указал ранее
