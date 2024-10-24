4f399c3cf8e956247f3749d23e7cf672f4eb03da
fullcringemaker <dmitriytrofimenko105@mail.ru>
2024-10-25 00:51:01 +0300

    Update lab3.asm

--- Lab3/lab3.asm
+++ Lab3/lab3.asm
@@ -1,18 +1,15 @@
 assume CS:code, DS:data
 
 data segment
-    ; Буферы для ввода строк
-    str_input1 db 20, 0, 20 dup(0) ; Первый байт - максимальная длина, второй - фактическая длина
+    str_input1 db 20, 0, 20 dup(0) 
     str_input2 db 20, 0, 20 dup(0)
 
     ten dw 10
 
-    ; Сообщения для вывода
     msg_null db 'null$', 0
     msg_pos db 'Position: $', 0
-    num_buf db 5 dup(0), '$', 0  ; Буфер для хранения позиции в виде строки
+    num_buf db 5 dup(0), '$', 0  
 
-    ; Перенос строки
     newline db 13, 10, '$'
 data ends
 
@@ -21,86 +18,68 @@ start:
     mov ax, data
     mov ds, ax
 
-    ; Вывод новой строки перед первым вводом
     mov ah, 09h
     lea dx, newline
     int 21h
 
-    ; Ввод первой строки
     lea dx, str_input1
     mov ah, 0Ah
     int 21h
 
-    ; Вывод новой строки после первого ввода
     mov ah, 09h
     lea dx, newline
     int 21h
 
-    ; Ввод второй строки
     lea dx, str_input2
     mov ah, 0Ah
     int 21h
 
-    ; Вывод новой строки после второго ввода
     mov ah, 09h
     lea dx, newline
     int 21h
 
-    ; Подготовка аргументов для функции strpbrk
-    lea ax, str_input2 + 2  ; Адрес начала второй строки
+    lea ax, str_input2 + 2  
     push ax
-    lea ax, str_input1 + 2  ; Адрес начала первой строки
+    lea ax, str_input1 + 2  
     push ax
 
-    ; Вызов функции strpbrk
     call strpbrk
 
-    ; Очистка стека после вызова функции
     add sp, 4
 
-    ; Проверка возвращенного значения
     cmp ax, 0
     je output_null
 
-    ; Вычисление позиции найденного символа
     lea bx, str_input1 + 2
-    sub ax, bx  ; AX = смещение найденного символа
-    inc ax      ; Позиция начинается с 1
-    mov bx, ax  ; Сохранение позиции в BX
+    sub ax, bx  
+    inc ax      
+    mov bx, ax  
 
-    ; Вывод 'Position: '
     mov ah, 09h
     lea dx, msg_pos
     int 21h
 
-    ; Вывод позиции
     call print_number
 
-    ; Вывод новой строки
     mov ah, 09h
     lea dx, newline
     int 21h
 
-    ; Переход к завершению программы
     jmp exit_program
 
 output_null:
-    ; Вывод 'null'
     mov ah, 09h
     lea dx, msg_null
     int 21h
 
-    ; Вывод новой строки
     mov ah, 09h
     lea dx, newline
     int 21h
 
 exit_program:
-    ; Завершение программы
     mov ah, 4Ch
     int 21h
 
-; Подпрограмма для вывода числа в BX
 print_number proc
     push ax
     push bx
@@ -108,17 +87,17 @@ print_number proc
     push dx
     push si
 
-    mov si, offset num_buf + 5  ; Указатель на конец буфера
-    mov byte ptr [si], '$'      ; Добавление символа конца строки
+    mov si, offset num_buf + 5  
+    mov byte ptr [si], '$'      
     dec si
 
-    mov cx, 0  ; Счетчик цифр
+    mov cx, 0  
 
 convert_loop:
     xor dx, dx
     mov ax, bx
-    div word ptr [ten]  ; Деление на 10
-    add dl, '0'         ; Преобразование остатка в ASCII
+    div word ptr [ten]  
+    add dl, '0'         
     mov [si], dl
     dec si
     inc cx
@@ -126,9 +105,8 @@ convert_loop:
     cmp bx, 0
     jne convert_loop
 
-    inc si  ; Указатель на первый символ числа
+    inc si  
 
-    ; Вывод числа
     mov ah, 09h
     mov dx, si
     int 21h
@@ -141,7 +119,6 @@ convert_loop:
     ret
 print_number endp
 
-; Реализация функции strpbrk
 strpbrk proc
     push bp
     mov bp, sp
@@ -151,28 +128,25 @@ strpbrk proc
     push cx
     push dx
 
-    mov si, [bp+4]  ; SI = указатель на str
-    mov di, [bp+6]  ; DI = указатель на sym
+    mov si, [bp+4]  
+    mov di, [bp+6]  
 
-    ; Получение длин строк
-    mov cl, [si - 1]  ; Длина str
+    mov cl, [si - 1]  
     mov ch, 0
-    mov bx, cx        ; BX = длина str
-    mov dl, [di - 1]  ; Длина sym
+    mov bx, cx        
+    mov dl, [di - 1]  
     mov dh, 0
 
-    ; Внешний цикл по str
 str_loop:
     cmp bx, 0
     je not_found
 
-    mov al, [si]  ; Текущий символ из str
+    mov al, [si]  
 
-    ; Внутренний цикл по sym
     push si
     push bx
 
-    mov cx, dx    ; CX = длина sym
+    mov cx, dx    
     mov di, [bp+6]
 
 sym_loop:
@@ -186,7 +160,6 @@ sym_loop:
     jmp sym_loop
 
 next_char:
-    ; Символ не найден, переходим к следующему
     pop bx
     pop si
     inc si
@@ -194,14 +167,13 @@ next_char:
     jmp str_loop
 
 found_char:
-    ; Символ найден
     pop bx
     pop si
-    mov ax, si    ; Возвращаем адрес найденного символа
+    mov ax, si    
     jmp strpbrk_end
 
 not_found:
-    mov ax, 0     ; Возвращаем NULL
+    mov ax, 0     
 
 strpbrk_end:
     pop dx
