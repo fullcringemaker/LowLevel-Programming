56927ecf6fec03569c58386aa63d36a40ce37dba
fullcringemaker <dmitriytrofimenko105@mail.ru>
2024-10-24 21:30:26 +0300

    Update lab3.asm

--- Lab3/lab3.asm
+++ Lab3/lab3.asm
@@ -2,19 +2,16 @@ assume CS:code, DS:data
 
 data segment
     ; Буферы для ввода строк
-    str_input1 db 20, 0, 20 dup(0) ; Первый байт - максимальная длина, второй - фактическая длина
-    str_input2 db 20, 0, 20 dup(0)
+    str_input1 db 255, 0, 255 dup(0) ; Первый байт - максимальная длина (255)
+    buffer_gap db 10 dup(0)          ; Промежуток между буферами
+    str_input2 db 255, 0, 255 dup(0)
 
     ten dw 10
 
     ; Сообщения для вывода
-    msg_prompt1 db 'Enter first string:$'
-    msg_prompt2 db 'Enter second string:$'
-    msg_null db 'null$'
-    msg_pos db 'Position: $'
-    num_buf db 5 dup(0), '$'
-
-    newline db 0Dh, 0Ah, '$'
+    msg_null db 'null$', 0
+    msg_pos db 'Position: $', 0
+    num_buf db 6 dup(0), '$', 0  ; Буфер для хранения позиции в виде строки
 data ends
 
 code segment
@@ -22,40 +19,32 @@ start:
     mov ax, data
     mov ds, ax
 
-    ; Вывод приглашения для ввода первой строки
-    mov ah, 9
-    lea dx, msg_prompt1
-    int 21h
-
     ; Ввод первой строки
     lea dx, str_input1
     mov ah, 0Ah
     int 21h
 
-    ; Вывод новой строки
-    mov ah, 9
-    lea dx, newline
-    int 21h
-
-    ; Вывод приглашения для ввода второй строки
-    mov ah, 9
-    lea dx, msg_prompt2
-    int 21h
+    ; Добавление символа конца строки '$' в первую строку
+    mov si, offset str_input1 + 2
+    mov cl, [str_input1 + 1]    ; Фактическая длина введенной строки
+    add si, cx
+    mov byte ptr [si], '$'
 
     ; Ввод второй строки
     lea dx, str_input2
     mov ah, 0Ah
     int 21h
 
-    ; Вывод новой строки
-    mov ah, 9
-    lea dx, newline
-    int 21h
+    ; Добавление символа конца строки '$' во вторую строку
+    mov si, offset str_input2 + 2
+    mov cl, [str_input2 + 1]
+    add si, cx
+    mov byte ptr [si], '$'
 
     ; Подготовка аргументов для функции strpbrk
-    lea ax, str_input2 + 2  ; Адрес начала второй строки
+    lea ax, str_input2 + 2      ; Адрес начала второй строки
     push ax
-    lea ax, str_input1 + 2  ; Адрес начала первой строки
+    lea ax, str_input1 + 2      ; Адрес начала первой строки
     push ax
 
     ; Вызов функции strpbrk
@@ -63,11 +52,6 @@ start:
 
     ; Очистка стека после вызова функции
     add sp, 4
-
-    ; Вывод новой строки перед результатом
-    mov ah, 9
-    lea dx, newline
-    int 21h
 
     ; Проверка возвращенного значения
     cmp ax, 0
@@ -155,54 +139,37 @@ strpbrk proc
     mov si, [bp+4]  ; SI = указатель на str
     mov di, [bp+6]  ; DI = указатель на sym
 
-    ; Получение длин строк
-    mov cl, [si - 2]  ; Длина str
-    mov ch, 0
-    mov bx, cx        ; BX = длина str
-    mov dl, [di - 2]  ; Длина sym
-    mov dh, 0
-
     ; Внешний цикл по str
 str_loop:
-    cmp bx, 0
-    je not_found
+    lodsb          ; AL = текущий символ из str, SI увеличивается
+    cmp al, '$'
+    je not_found   ; Дошли до конца строки
 
-    mov al, [si]  ; Текущий символ из str
+    push si        ; Сохраняем SI
 
     ; Внутренний цикл по sym
-    push si
-    push bx
-
-    mov cx, dx    ; CX = длина sym
-    mov di, [bp+6]
-
+    mov di, [bp+6] ; Восстанавливаем DI
 sym_loop:
-    cmp cx, 0
-    je next_char
-    mov ah, [di]
-    cmp al, ah
+    mov bl, [di]
+    cmp bl, '$'
+    je end_sym_loop
+    cmp al, bl
     je found_char
     inc di
-    dec cx
     jmp sym_loop
 
-next_char:
-    ; Символ не найден, переходим к следующему
-    pop bx
-    pop si
-    inc si
-    dec bx
+end_sym_loop:
+    pop si         ; Восстанавливаем SI
     jmp str_loop
 
 found_char:
-    ; Символ найден
-    pop bx
-    pop si
-    mov ax, si    ; Возвращаем адрес найденного символа
+    pop si         ; Восстанавливаем SI
+    dec si         ; Корректируем SI после LODSB
+    mov ax, si     ; Возвращаем адрес найденного символа
     jmp strpbrk_end
 
 not_found:
-    mov ax, 0     ; Возвращаем NULL
+    mov ax, 0      ; Возвращаем NULL
 
 strpbrk_end:
     pop dx
