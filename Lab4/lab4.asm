ad1aaeacbd45083b3e98b6bc7e2a18c49e71472a
fullcringemaker <dmitriytrofimenko105@mail.ru>
2024-11-14 18:42:31 +0300

    Update lab4.asm

--- Lab4/lab4.asm
+++ Lab4/lab4.asm
@@ -1,93 +1,84 @@
-
 assume CS:code, DS:data
 
 data segment
     msg db "Hello, world!$"
-    var1 dd 12345678h   ; Double-word variable
-    var2 dd 0           ; Double-word variable to receive data
-data ends
-
-PUSHM MACRO X
-    IFDEF X
-        IF TYPE X EQ 4
-            MOV EAX, X      ; Move double-word variable into EAX
-            PUSH EAX        ; Push EAX onto the stack
-        ELSE
-            .ERR <Variable X is not a double-word (32-bit) variable>
-        ENDIF
-    ELSE
-        .ERR <Variable X is not defined>
-    ENDIF
-ENDM
-
-POPM MACRO X
-    IFDEF X
-        IF TYPE X EQ 4
-            POP EAX         ; Pop value from stack into EAX
-            MOV X, EAX      ; Move value from EAX into variable X
-        ELSE
-            .ERR <Variable X is not a double-word (32-bit) variable>
-        ENDIF
-    ELSE
-        .ERR <Variable X is not defined>
-    ENDIF
-ENDM
-
-CALLM MACRO P
-    IFDEF P
-        CALL P              ; Call procedure P
-    ELSE
-        .ERR <Procedure P is not defined>
-    ENDIF
-ENDM
-
-RETM MACRO N
-    IFCONST N
-        RET N               ; Return and clean up N bytes from the stack
-    ELSE
-        .ERR <N must be a constant>
-    ENDIF
-ENDM
-
-LOOPM MACRO L
-    IFDEF L
-        DEC EBX             ; Decrement EBX (loop counter)
-        JNZ L               ; Jump to label L if EBX is not zero
-    ELSE
-        .ERR <Label L is not defined>
-    ENDIF
-ENDM
+    var dd 0       ; Переменная типа двойное слово (4 байта)
+    data ends
 
 code segment
 start:
     mov AX, data
     mov DS, AX
 
-    ; Demonstrate PUSHM and POPM
-    MOV EAX, 12345678h
-    MOV var1, EAX
-
-    PUSHM var1      ; Push var1 onto the stack
-
-    ; Modify var1
-    MOV var1, 0
-
-    POPM var2       ; Pop value from stack into var2
-
-    ; Demonstrate LOOPM
-    MOV EBX, 5      ; Initialize loop counter to 5
-    MOV ECX, 0      ; Initialize ECX to zero
-
-loop_start:
-    INC ECX         ; Increment ECX
-    LOOPM loop_start
-
-    ; Use DOS interrupt to print message
+    ; Вывод сообщения
     mov AH, 09h
     mov DX, offset msg
     int 21h
 
-    mov AH, 4Ch     ; Exit to DOS
+    ; Пример использования макроса PUSHM
+    mov EAX, 12345678h  ; Поместим значение в EAX для теста
+    PUSHM var           ; Используем макрос для записи в переменную типа двойное слово
+
+    ; Пример использования макроса POPM
+    POPM var            ; Используем макрос для чтения значения из переменной
+
+    ; Пример использования макроса CALLM
+    CALLM MyProc        ; Вызов процедуры с помощью макроса
+
+    ; Пример использования макроса RETM
+    RETM 0              ; Возврат из процедуры с аргументом 0
+
+    ; Пример использования макроса LOOPM
+    LOOPM LoopLabel     ; Цикл с использованием макроса
+
+    ; Завершение программы
+    mov AH, 4Ch
     int 21h
+
+; Макросы
+PUSHM MACRO X
+    ; Проверяем существование переменной X
+    ; (на уровне компилятора, можно добавлять свои проверки на этапе компиляции)
+    ; Сохраняем значение переменной X в памяти
+    mov EAX, X         ; Загружаем значение переменной X в регистр EAX
+    push EAX           ; Сохраняем значение EAX на стеке
+ENDM
+
+POPM MACRO X
+    ; Проверяем существование переменной X
+    ; Восстанавливаем значение из стека в переменную X
+    pop EAX            ; Извлекаем значение из стека в регистр EAX
+    mov X, EAX         ; Сохраняем его в переменную X
+ENDM
+
+CALLM MACRO P
+    ; Проверяем существование процедуры P
+    ; Переход к процедуре P
+    call P             ; Вызываем процедуру по имени P
+ENDM
+
+RETM MACRO N
+    ; Проверяем существование константы N
+    ; Возврат из процедуры с указанием количества байт для очистки стека
+    ret N              ; Возвращаем из процедуры с очисткой N байт
+ENDM
+
+LOOPM MACRO L
+    ; Проверяем существование метки L
+    ; Организуем цикл
+    loop L             ; Выполняем цикл до достижения метки L
+ENDM
+
+; Процедура для примера использования CALLM
+MyProc PROC
+    ; Простая процедура для теста
+    mov AX, 1
+    ret
+MyProc ENDP
+
+LoopLabel:
+    ; Место назначения для цикла
+    mov CX, 10         ; Пример количества итераций
+    loop LoopLabel     ; Цикл по метке
 code ends
 end start
