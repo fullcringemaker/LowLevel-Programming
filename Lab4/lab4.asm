6b79e61fe04e7e0c5217f2d11f9ea2a3ef0e8c03
fullcringemaker <dmitriytrofimenko105@mail.ru>
2024-11-15 22:49:30 +0300

    Update lab4.asm

--- Lab4/lab4.asm
+++ Lab4/lab4.asm
@@ -1,33 +1,41 @@
+assume CS:code, DS:data
+
 PUSHM macro X
-    push word ptr X+2    
-    push word ptr X     
+    push word ptr X+2
+    push word ptr X
 endm
 
 POPM macro X
-    pop word ptr X        
-    pop word ptr X+2     
+    pop word ptr X
+    pop word ptr X+2
 endm
 
 CALLM macro P
-    call P
+    push AX              ; Сохраняем AX
+    push offset after_callm@
+    jmp P
+    after_callm@:
+    pop AX               ; Восстанавливаем AX
 endm
 
 RETM macro N
     add SP, N
-    ret
+    pop AX
+    jmp AX
 endm
 
 LOOPM macro L
-    loop L
+    push CX              ; Сохраняем CX
+    dec CX
+    jnz L
+    pop CX               ; Восстанавливаем CX
 endm
 
-assume CS:code, DS:data
-
 data segment
-    msg    db "Hello, world!$"  
-    var    dw 5678h               
-           dw 1234h               
-    count  dw 5                   
+    msg    db "Hello, world!$"
+    var    dw 5678h
+           dw 1234h
+    count  dw 5
 data ends
 
 code segment
@@ -39,19 +47,20 @@ start:
     mov DX, offset msg
     int 21h
 
-    pushm var
+    PUSHM var
 
-    mov word ptr var, 0ABCDh       
-    mov word ptr var+2, 0EF01h     
+    mov word ptr var, 0ABCDh
+    mov word ptr var+2, 0EF01h
 
-    popm var
+    POPM var
 
     CALLM my_procedure
 
-    mov CX, count                  
+    mov CX, count
+
 loop_start:
-    mov AH, 02h                    
-    mov DL, 'A'                    
+    mov AH, 02h
+    mov DL, 'A'
     int 21h
 
     LOOPM loop_start
@@ -60,11 +69,10 @@ loop_start:
     int 21h
 
 my_procedure:
-    mov AH, 02h                    
-    mov DL, 'P'                    
+    mov AH, 02h
+    mov DL, 'P'
     int 21h
-
-    retm 0
+    RETM 0
 code ends
 
 end start
