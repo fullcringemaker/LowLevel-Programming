7f920dea0f708e4516e62928bb6a3e81d5e5d051
fullcringemaker <dmitriytrofimenko105@mail.ru>
2024-11-15 22:18:54 +0300

    Update lab4withcom.asm

--- Lab4/lab4withcom.asm
+++ Lab4/lab4withcom.asm
@@ -1,92 +1,74 @@
-PUSHM macro X
-    ; Проверка существования переменной X производится на этапе компиляции
-    ; Предполагается, что X занимает два слова (4 байта)
-    push word ptr X+2    ; Пушим старшее слово
-    push word ptr X      ; Пушим младшее слово
-endm
-
-; Макрос POPM X - выталкивает двойное слово из стека в X
-POPM macro X
-    ; Проверка существования переменной X производится на этапе компиляции
-    pop word ptr X        ; Выталкиваем младшее слово
-    pop word ptr X+2      ; Выталкиваем старшее слово
-endm
-
-; Макрос CALLM P - вызывает процедуру P
-CALLM macro P
-    ; Проверка существования метки P производится на этапе компиляции
-    call P
-endm
-
-; Макрос RETM N - возвращается из процедуры, удаляя N байт из стека
-RETM macro N
-    add SP, N
-    ret
-endm
-
-; Макрос LOOPM L - выполняет цикл к метке L, используя CX как счетчик
-LOOPM macro L
-    loop L
-endm
-
-; Определение сегментов
 assume CS:code, DS:data
 
+PUSHM macro X
+    push word ptr X+2
+    push word ptr X
+endm
+
+POPM macro X
+    pop word ptr X
+    pop word ptr X+2
+endm
+
+CALLM macro P
+    push offset after_callm@
+    jmp P
+    after_callm@:
+endm
+
+RETM macro N
+    add SP, N
+    pop AX
+    jmp AX
+endm
+
+LOOPM macro L
+    dec CX
+    jnz L
+endm
+
 data segment
-    msg    db "Hello, world!$"   ; Сообщение для вывода
-    var    dw 5678h               ; Первое слово двойного слова
-           dw 1234h               ; Второе слово двойного слова
-    count  dw 5                   ; Счетчик для цикла
+    msg    db "Hello, world!$"
+    var    dw 5678h
+           dw 1234h
+    count  dw 5
 data ends
 
 code segment
 start:
-    ; Инициализация сегмента данных
     mov AX, data
     mov DS, AX
 
-    ; Вывод строки на экран
     mov AH, 09h
     mov DX, offset msg
     int 21h
 
-    ; Демонстрация использования макроса PUSHM
-    pushm var
+    PUSHM var
 
-    ; Изменение значения переменной var
-    mov word ptr var, 0ABCDh       ; Изменяем младшее слово
-    mov word ptr var+2, 0EF01h     ; Изменяем старшее слово
+    mov word ptr var, 0ABCDh
+    mov word ptr var+2, 0EF01h
 
-    ; Демонстрация использования макроса POPM для восстановления var
-    popm var
+    POPM var
 
-    ; Демонстрация использования макроса CALLM
     CALLM my_procedure
 
-    ; Демонстрация использования макроса LOOPM
-    mov CX, count                  ; Устанавливаем счетчик цикла
+    mov CX, count
+
 loop_start:
-    ; Пример кода внутри цикла: вывод символа 'A'
-    mov AH, 02h                    ; Функция вывода символа
-    mov DL, 'A'                    ; Символ для вывода
+    mov AH, 02h
+    mov DL, 'A'
     int 21h
 
-    ; Вызов макроса LOOPM для перехода к loop_start
     LOOPM loop_start
 
-    ; Завершение программы
     mov AH, 4Ch
     int 21h
 
-; Процедура для примера вызова
 my_procedure:
-    ; Пример кода процедуры: вывод символа 'P'
-    mov AH, 02h                    ; Функция вывода символа
-    mov DL, 'P'                    ; Символ для вывода
+    mov AH, 02h
+    mov DL, 'P'
     int 21h
-
-    ; Возврат из процедуры с очисткой стека (если необходимо)
-    retm 0
+    RETM 0
 code ends
 
 end start
